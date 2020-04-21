5dc5c01368f9145a41d0ba27e197edfcffe24d3a
RenatoWw <renatoflud@protonmail.com>
2020-04-21 14:58:50 +0000

    modularization of classes

---
+++ citizen.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void citizen(char name[20], int action) {
+    printf("%s é um cidadão morador do reino de Eruthe", name);
+}
--- main.c
+++ main.c
@@ -1,11 +1,9 @@
-#include <stdio.h>
 
 // TODO
 /*
 input validation
 name char limit
 help menu
-module for each class: knight.h - knight path
 kingdom and king name generator
 option for generate protagonist name
 more classes
@@ -39,17 +37,19 @@ __________  x . y ascii (generated square)
 ‾‾‾‾‾‾‾‾‾‾
 -------------------
 */
-void knight();
-void mage();
-void citizen();
-char nome[20];
-int action;
+
+#include <stdio.h>
+#include "knight.h"
+#include "mage.h"
+#include "citizen.h"
 
 int main() {
+    char name[20];
+    int action;
     puts("Bem-vindo ao text adventure game feito em C");
     puts("Qual é o nome de seu personagem?");
     printf("> ");
-    scanf("%s", nome);
+    scanf("%s", name);
     puts("Escolha uma classe:");
     puts("1 - Cavaleiro");
     puts("2 - Mago");
@@ -58,92 +58,14 @@ int main() {
     scanf("%d", &action);
     switch(action) {
         case 1:
-            knight();
+            knight(name, action);
             break;
         case 2:
-            mage();
+            mage(name, action);
             break;
         case 3:
-            citizen();
+            citizen(name, action);
             break;
     }
     return 0;
-}
-
-void knight() {
-    printf("%s é um cavaleiro do reino de Eruthe, sua missão de acordo com o Rei Yeoman Renoldus é matar um orc que está se escondendo na floresta.\n", nome);
-    while(1) {
-        puts("O que deseja fazer?");
-        puts("1 - Falar com o Rei");
-        puts("2 - Ir para floresta");
-        puts("3 - Olhar ao redor");
-        printf("> ");
-        scanf("%d", &action);
-        if(action == 1) {
-            puts("Você pergunta ao Rei o que precisa fazer, ele diz que a sua tarefa é matar um orc escondido na floresta em troca de uma recompensa.");
-            continue;
-        } else if(action == 2) {
-            puts("Você vai para a floresta procurando pelo orc, você se depara com um cachorro com a pata presa em uma armadilha de urso, o que você faz?");
-            break;
-        } else {
-            puts("Vocẽ está no palácio do Rei Yeoman Renoldus.");
-            continue;
-        }
-    }
-    while(1) {
-        puts("1 - Libertar o cachorro da armadilha");
-        puts("2 - Seguir o seu caminho e esquecer o cachorro");
-        printf("> ");
-        scanf("%d", &action);
-        if(action == 1) {
-            puts("Você libertou o cachorro da armadilha de urso, ele se lembrará disso.");
-            puts("Depois de libertar o cachorro, você segue o seu caminho e se depara com o orc.");
-            break;
-        } else {
-            puts("Você seguiu o seu caminho sem ajudar o cachorro, ele se lembrará disso.");
-            puts("Após seguir sem ajudar o cachorro, você se depara com o orc.");
-            break;
-        }
-    }
-}
-
-void mage() {
-    printf("%s é um mago do reino de Eruthe, sua missão de acordo com o Rei Yeoman Renoldus é exterminar um bando de minotauros em uma torre perto do reino.\n", nome);
-    while(1) {
-        puts("O que deseja fazer?");
-        puts("1 - Falar com o Rei");
-        puts("2 - Seguir para a torre");
-        puts("3 - Olhar ao seu redor");
-        printf("> ");
-        scanf("%d", &action);
-        if(action == 1) {
-            puts("Você pergunta ao Rei o que precisa fazer, ele diz que a sua tarefa é exterminar um bando de minotauros perto dali");
-            continue;
-        } else if(action == 2) {
-            puts("Você segue para a torre, no caminho encontra uma caverna, com uma voz no profundo escuro da caverna exclamando por ajuda.\nO que você faz?");
-            break;
-        } else {
-            puts("Você está no palácio do Rei Yeoman Renoldus.");
-            continue;
-        }
-    }
-    while(1) {
-        puts("1 - Entrar na escura caverna");
-        puts("2 - Evitar a caverna e ir para a torre");
-        printf("> ");
-        scanf("%d", &action);
-        if(action == 1) {
-            puts("Você entra na caverna, por um momento a voz pedindo por socorro desaparece. Você se vê ali no escuro sem saber o que fazer...");
-            puts("Até que você ouve uma voz baixinho, em um canto sombrio, por um momento pensa que aquela voz era a que estava pedindo por ajuda.");
-            break;
-        } else {
-            puts("Você segue para a torre e encontra o bando de minotauros, você extermina todos, com excessão de um, o chefão.");
-            break;
-        }
-    }
-
-}
-
-void citizen() {
-    printf("%s é um cidadão morador do reino de Eruthe", nome);
 }
