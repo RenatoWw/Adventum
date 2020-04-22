7ff57ef29cb0566f80bb73a96de49b7096303c0f
RenatoWw <renatoflud@protonmail.com>
2020-04-22 15:10:53 +0000

    help menu

--- main.c
+++ main.c
@@ -40,12 +40,28 @@ __________  x . y ascii (generated square)
 */
 
 #include <stdio.h>
+#include <unistd.h>
 #include "knight.h"
 #include "mage.h"
 #include "citizen.h"
 
+void playGame();
+void showHelp();
+
 int main() {
+    char help;
     puts("Bem-vindo ao text adventure game feito em C");
+    puts("Aperte h para mostrar o menu de ajuda e sobre.");
+    puts("Aperte qualquer tecla para começar o jogo.");
+    help = getchar();
+    if(help == 'h') {
+        showHelp();
+    } else {
+        playGame();
+    }
+}
+
+void playGame() {
     puts("Qual é o nome de seu personagem?");
     printf("> ");
     scanf("%s", name);
@@ -57,13 +73,24 @@ int main() {
     scanf("%d", &action);
     switch(action) {
         case 1:
-            knight(name, action);
-            break;
+        knight(name, action);
+        break;
         case 2:
-            mage(name, action);
-            break;
+        mage(name, action);
+        break;
         case 3:
-            citizen(name, action);
-            break;
+        citizen(name, action);
+        break;
     }
+}
+
+void showHelp() {
+    printf("Adventum é um text adventure game escrito principalmente em C e foi desenvolvido para aprendizado e revisão de conceitos básicos. ");
+    printf("A jogabilidade dele é simples, basta apertar os números do teclado para selecionar uma ação ou uma fala quando falando com NPCs. ");
+    printf("Em breve irá ter implementação de lutas com bosses, monstros, lojas para comprar poções e equipamentos especiais, e uma história bem mais trabalhada. ");
+    printf("Como todo projeto \"grande\" não surge de um dia para o outro, vai demorar um pouco para tudo isso ser implementado, mas de pouco em pouco será finalizado. ");
+    printf("O código deste jogo pode ser modificado e distribuido livremente, não tem objetivos comerciais, apenas para aprendizado como mencionado anteriormente.\n");
+    puts("\nIniciando jogo...");
+    sleep(3);
+    playGame();
 }
